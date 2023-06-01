c94cf310f3991b7fdd6aa107c36c2c220cd0e7bf
Amar Oklopcic <amar_oklopcic@live.com>
2023-06-01 17:46:51 -0500

    refactor .roulette and add back refactored .driveby and .grenade

--- lib/cogs/cog.py
+++ lib/cogs/cog.py
@@ -5,6 +5,10 @@ from discord.ext import commands
 
 
 class CommonCog(commands.Cog):
+    emoji_ack = ":hourglass_flowing_sand:"
+    emoji_finish = ":ballot_box_with_check:"
+    emoji_error = ":no_entry_sign:"
+
     def __init__(self, bot: commands.Bot) -> None:
         super().__init__()
         self.bot = bot
@@ -12,19 +16,24 @@ class CommonCog(commands.Cog):
     async def sleep(self, duration: float):
         await asyncio.sleep(duration)
 
-    async def acknowledge(self, context: commands.Context):
-        """Adds a timer emoji to users issued command."""
-        await context.message.add_reaction("⏳")
+    async def acknowledge(self, ctx: commands.Context):
+        """Adds `emoji_ack` to users issued command."""
+        await ctx.message.add_reaction(self.emoji_ack)
         # TODO: check if user has sufficient privileges
         # for role in ctx.message.author.roles:
         #     if role.permissions.manage_permissions is not True:
         #         await ctx.message.add_reaction("🚫")
         #         return
 
-    async def finish(self, context: commands.Context):
-        """Removes timer emoji and adds checkmark emoji to users issued command."""
-        await context.message.remove_reaction("⏳", self.bot.user)
-        await context.message.add_reaction("☑")
+    async def finish(self, ctx: commands.Context):
+        """Removes `emoji_ack` and adds `emoji_finish` to users issued command."""
+        await ctx.message.remove_reaction(self.emoji_ack, self.bot.user)
+        await ctx.message.add_reaction(self.emoji_finish)
+
+    async def react_error(self, ctx: commands.Context):
+        """Removes `emoji_ack` and adds `emoji_error` to users issued command."""
+        await ctx.message.remove_reaction(self.emoji_ack, self.bot.user)
+        await ctx.message.add_reaction(self.emoji_error)
 
     async def join_authors_vc(self, ctx: commands.Context):
         """Joins a users voice channel (`ctx.author.voice.channel`)."""
--- lib/cogs/memes.py
+++ lib/cogs/memes.py
@@ -13,23 +13,115 @@ class MemeCog(CommonCog):
     # -vvv- commands suggested by me (very, very annoying) -vvv-
     @commands.command()
     async def roulette(self, ctx: commands.Context):
-        """Randomly selects a user and disconnects them from the channel, while playing a gunshot sound."""
+        """Plays a gunshot sounds and kicks a random user from the voice channel."""
         self.acknowledge(ctx)
 
-        vc = ctx.message.author.voice.channel
+        # validate user is in voice channel
+        voice_channel = None
+        if ctx.author.voice:
+            voice_channel = ctx.author.voice.channel
+        else:
+            await self.react_error(ctx)
+            return
 
-        if len(vc.members) > 0:
-            chosen_one = random.choice(vc.members)
-            gun_sound = YTDLSource.from_file("sounds/roulette.wav")
+        if len(voice_channel.members) < 1:
+            await self.react_error(ctx)
+            return
 
-            # connect to voice channel and play sound
-            voice_client = await self.join_vc(ctx, channel=vc)
-            voice_client.play(gun_sound)
+        gun_sound = YTDLSource.from_file("sounds/roulette.wav")
 
-            # sleep so user can hear gunshot before they go
-            await self.sleep(gun_sound.data["duration"])
+        # connect to voice channel and play sound
+        voice_client = await self.join_authors_vc(ctx)
+        voice_client.play(gun_sound)
 
-            await chosen_one.edit(voice_channel=None)
+        # sleep so user can hear gunshot before they go
+        await self.sleep(gun_sound.data["duration"])
 
-            await self.disconnect_vc(ctx)
-            await self.finish(ctx)
+        chosen_one = random.choice(voice_channel.members)
+        await chosen_one.edit(voice_channel=None)
+
+        await self.disconnect_vc(ctx)
+        await self.finish(ctx)
+
+    @commands.command()
+    async def driveby(self, ctx: commands.Context):
+        """Plays machine gun sound while kicking multiple people from the voice channel."""
+        self.acknowledge(ctx)
+
+        # validate user is in voice channel
+        voice_channel = None
+        if ctx.author.voice:
+            voice_channel = ctx.author.voice.channel
+        else:
+            await self.react_error(ctx)
+            return
+
+        if len(voice_channel.members) < 1:
+            await self.react_error(ctx)
+            return
+
+        # join voice_channel and play machine gun sound
+        machine_gun_sound = await YTDLSource.from_url("sounds/machine_gun.wav")
+
+        voice_client = await self.join_authors_vc(ctx)
+        voice_client.play(machine_gun_sound)
+
+        # initial wait so everyone can here the "CHK CHK"
+        await self.sleep(1)
+
+        targets = [member for member in voice_channel.members if not member.bot]
+
+        # keep going until everyone has been kicked
+        while targets:
+            await self.sleep(random.uniform(1, 2))
+            user = random.choice(targets)
+            await user.edit(voice_channel=None)
+            targets.remove(user)
+            if len(targets) == 0:
+                break
+
+        await self.disconnect_vc(ctx)
+        await self.finish(ctx)
+
+    @commands.command()
+    async def grenade(self, ctx: commands.Context):
+        """Plays grenade sound while everyone is scattered across various channels."""
+        self.acknowledge(ctx)
+
+        # validate user is in voice channel
+        voice_channel = None
+        if ctx.author.voice:
+            voice_channel = ctx.author.voice.channel
+        else:
+            await self.react_error(ctx)
+            return
+
+        if len(voice_channel.members) < 1:
+            await self.react_error(ctx)
+            return
+
+        # join voice_channel and play grenade sound
+        grenade_sound = await YTDLSource.from_url("sounds/grenade_oh_fudge.wav")
+
+        voice_client = await self.join_authors_vc(ctx)
+        voice_client.play(grenade_sound)
+
+        all_voice_channels = voice_channel.guild.voice_channels
+
+        # sleep so users can hear grenade and "OH FUDGE"
+        await self.sleep(3)
+
+        # avoid kicking them into a channel they dont have access to
+        for member in voice_channel.members:
+            if member.bot:
+                continue
+
+            available_channels = []
+            for channel in all_voice_channels:
+                if channel.permissions_for(member).connect:
+                    available_channels.append(channel)
+
+            await member.edit(voice_channel=random.choice(available_channels))
+
+        await self.disconnect_vc(ctx)
+        await self.finish(ctx)
