b0261a5b6f518267e2b9ae2f20bbbb814c81e1f0
Amar Oklopcic <aoklo.pub@pm.me>
2023-06-19 12:44:03 -0500

    update quote of the day cronjob to run at 12pm UTC (7am CST)

--- lib/cogs/quotes.py
+++ lib/cogs/quotes.py
@@ -23,7 +23,7 @@ class Quotes(commands.Cog):
         self.base_url = "https://quotes.rest"
         self.api_token = os.environ.get("THEYSAIDSO_API_TOKEN")
         self.qod_cron = aiocron.crontab(
-            "30 12 * * *", func=self.qod, loop=self.bot.loop, tz=timezone.utc
+            "0 12 * * *", func=self.qod, loop=self.bot.loop, tz=timezone.utc
         )
 
     async def make_request(self, url: str, method: str = "GET", **kwargs):
