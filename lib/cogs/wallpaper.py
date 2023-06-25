a1efceb99ad29d11f65b9d01050a4ed418855f9a
Amar Oklopcic <amar_oklopcic@live.com>
2023-06-25 12:54:30 -0500

    add wip wallpaper cog

---
+++ lib/cogs/wallpaper.py
@@ -0,0 +1,150 @@
+import os
+import random
+from datetime import timezone
+from typing import List, Literal, Optional
+
+import aiocron
+import aiohttp
+import cloudscraper
+from bs4 import BeautifulSoup
+from discord import Interaction, app_commands
+from discord.ext import commands
+
+from lib.logging import get_logger
+
+
+class DailyWallpapersCog(commands.GroupCog, group_name="walls"):
+    """Fetches wallpapers daily and posts them to a `#wallpapers` channel if it exists."""
+
+    def __init__(self, bot: commands.Bot) -> None:
+        super().__init__()
+
+        self.logger = get_logger(__name__)
+        self.logger.debug("Initializing DailyWallpapersCog...")
+
+        self.bot = bot
+        self.cron_job = aiocron.crontab(
+            "* * * * *", func=self.post_wallpaper, loop=self.bot.loop, tz=timezone.utc
+        )
+
+    async def make_request(self, url: str, method: str = "GET", **kwargs):
+        session = aiohttp.ClientSession()
+
+        try:
+            resp = await session.request(
+                method=method,
+                url=url,
+                **kwargs,
+            )
+            await session.close()
+            resp.raise_for_status()
+            return resp
+        except (
+            aiohttp.ClientError,
+            aiohttp.http_exceptions.HttpProcessingError,
+        ) as e:
+            status = getattr(e, "status", None)
+            message = getattr(e, "message", None)
+            self.logger.exception(
+                f"aiohttp exception occurred - {url} [{status}]: {message}"
+            )
+            raise
+        except Exception as e:
+            self.logger.exception("Non-aiohttp exception occurred")
+            raise
+
+    async def fetch_wallpapers(
+        self,
+        category: List[Literal["general", "anime", "people"]],
+        purity: List[Literal["sfw", "sketchy"]],
+        ai_art: bool | None = None,
+    ):
+        # https://wallhaven.cc/search
+        # ?categories=101
+        # &purity=100
+        # &atleast=2560x1440
+        # &ratios=landscape
+        # &sorting=random
+        # &order=desc
+        # &ai_art_filter=0
+        base_url = "https://wallhaven.cc/search"
+
+        categories_param = (
+            f"{'general' in category and 1 or 0}"
+            f"{'anime' in category and 1 or 0}"
+            f"{'people' in category and 1 or 0}"
+        )
+        purity_param = f"{'sfw' in purity and 1 or 0}{'sketchy' in purity and 1 or 0}0"
+
+        params = {
+            "categories": categories_param,
+            "purity": purity_param,
+            "atleast": "2560x1440",
+            "ratios": "landscape",
+            "sorting": "random",
+            "order": "desc",
+        }
+
+        if ai_art is not None:
+            params["ai_art_filter"] = f"{ai_art is True and 1 or 0}"
+
+        try:
+            scraper = cloudscraper.create_scraper()
+            resp = scraper.get(url=base_url, params=params)
+            print(resp)
+
+            # TODO: delete print statements
+            text = resp.text
+
+            soup = BeautifulSoup(text)
+            thumbs_section = soup.find("section", class_="thumb-listing-page")
+            thumbnails = thumbs_section.ul.find_all("li")
+            self.logger.debug(f"found {len(thumbnails)} image thumbnails")
+
+            image_urls = []
+            for thumbnail in thumbnails:
+                figure = thumbnail.find("figure")
+                image_id = figure.get("data-wallpaper-id")
+                image_group = image_id[:2]
+                info = figure.find("div", class_="thumb-info")
+
+                # get image extention
+                image_ext = None
+                if info.find("span", class_="png"):
+                    # check to make sure image is not a png
+                    image_ext = "png"
+                else:
+                    data_src = thumbnail.find("figure").find("img").get("data-src")
+                    image_ext = data_src.split(".")[-1]
+
+                endpoint = f"/full/{image_group}/wallhaven-{image_id}.{image_ext}"
+                full_image_url = "https://w.wallhaven.cc" + endpoint
+                print(full_image_url)
+
+                image_urls.append(full_image_url)
+
+            scraper.close()
+
+            return image_urls
+
+        except Exception as e:
+            self.logger.exception("Exception when parsing wallpapers")
+            raise
+
+    async def post_daily_wall(self):
+        """Posts a daily wallpaper to the `#wallpapers` channel."""
+        self.logger.debug("Posting daily wallpaper...")
+
+        channel = "wallpapers"
+        image_urls = await self.fetch_wallpapers(category=["general"], purity=["sfw"])
+
+    @app_commands.command()
+    async def random(self, interaction: Interaction):
+        """Sends a random high-quality wall paper."""
+        self.logger.debug("Sending wallpaper...")
+
+        await interaction.response.defer()
+
+        image_urls = await self.fetch_wallpapers(category=["general"], purity=["sfw"])
+
+        await interaction.followup.send(random.choice(image_urls))
